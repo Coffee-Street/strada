1cde03cc0f61f1350073a61e735df718b89d05fb
wnsgml972 <wnsgml972@gmail.com>
2021-04-24 16:34:48 +0900

    Update jar

--- api/src/test/kotlin/com/wnsgml972/strada/ArchunitApplicationTest.kt
+++ api/src/test/kotlin/com/wnsgml972/strada/ArchunitApplicationTest.kt
@@ -3,7 +3,6 @@ package com.wnsgml972.strada
 import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.core.importer.ClassFileImporter
 import com.tngtech.archunit.core.importer.ImportOption
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,14 +17,6 @@ class ArchunitApplicationTest {
     private val importedClasses: JavaClasses = ClassFileImporter()
         .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
         .importPackages("com.wnsgml972.strada")
-
-    @Test
-    fun `service 클래스는 controller에서만 접근해야 합니다`() {
-        classes()
-            .that().resideInAPackage("..service..")
-            .should().onlyBeAccessed().byAnyPackage("..service..", "..api..")
-            .check(importedClasses)
-    }
 
     @Test
     fun `field injection을 사용하면 안됩니다`() {
