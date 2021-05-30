b061ac8d0a57e16c2244e0623d451504200e7829
wnsgml972 <wnsgml972@gmail.com>
2021-05-30 16:48:21 +0900

    Refactor test object

--- api/src/test/kotlin/com/wnsgml972/strada/IntegrationTest.kt
+++ api/src/test/kotlin/com/wnsgml972/strada/IntegrationTest.kt
@@ -2,6 +2,7 @@ package com.wnsgml972.strada
 
 import com.wnsgml972.strada.config.StradaDomainConfig
 import com.wnsgml972.strada.config.StradaWebConfig
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.context.annotation.Import
@@ -13,4 +14,5 @@ import org.springframework.test.context.ActiveProfiles
     StradaDomainConfig::class,
     StradaWebConfig::class,
 )
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IntegrationTest : AbstractWebTest()
