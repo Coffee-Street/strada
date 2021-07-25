533f0eb913d1ccec609ffdcd9631462032b4684f
wnsgml972 <wnsgml972@gmail.com>
2021-07-25 13:49:28 +0900

    Fix test

--- api/src/test/kotlin/com/wnsgml972/strada/AuthHelper.kt
+++ api/src/test/kotlin/com/wnsgml972/strada/AuthHelper.kt
@@ -3,7 +3,9 @@ package com.wnsgml972.strada
 import com.wnsgml972.strada.api.v1.account.service.AccessTokenRequest
 import com.wnsgml972.strada.api.v1.account.service.JwtService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 
+@Component
 class AuthHelper @Autowired constructor(
     private val jwtService: JwtService
 ) {
